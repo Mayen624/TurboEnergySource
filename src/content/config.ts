a01b3761f424778b05c786d02442735e97b47631
Mayen624 <juliomayen300@gmail.com>
2025-11-24 00:02:20 -0600

    Remove unused blog collection and pages
      - Remove src/pages/en/blog/ directory
      - Remove blog collection from content config
      - Remove src/content/blog/ directory
      - Fix build error: collection 'blog' does not exist

--- src/content/config.ts
+++ src/content/config.ts
@@ -59,24 +59,6 @@ const productsCollection = defineCollection({
   }),
 });
 
-const blogCollection = defineCollection({
-  type: "content",
-  schema: ({ image }) => z.object ({
-  title: z.string(),
-  description: z.string(),
-  contents: z.array(z.string()),
-  author: z.string(),
-  role: z.string().optional(),
-  authorImage: image(),
-  authorImageAlt: z.string(),
-  pubDate: z.date(),
-  cardImage: image(),
-  cardImageAlt: z.string(),
-  readTime: z.number(),
-  tags: z.array(z.string()).optional(),
-  }),
-});
-
 const insightsCollection = defineCollection({
   type: "content",
   schema: ({ image }) => z.object ({
@@ -91,6 +73,5 @@ const insightsCollection = defineCollection({
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
   'products': productsCollection,
-  'blog': blogCollection,
   'insights': insightsCollection,
 };
